bdebdd6e67ef90e88900a8e6b1a151ffee0a5938
dharmsavaliya <dharmsavaliya5@gmail.com>
2024-09-05 09:31:09 +0530

    Initial commit

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dashborad  extends StatelessWidget {
+  const Dashborad ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _SplashScreenState extends StatefulWidget {
+  const _SplashScreenState({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _SplashScreenState extends State<_SplashScreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
